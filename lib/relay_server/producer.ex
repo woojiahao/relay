095ad1177e397adce6b101a1bfdc21938e344034
woojiahao <woojiahao1234@gmail.com>
2021-03-14 11:53:13 +0800

    Use JSON requests for producer messages

--- lib/relay_server/producer.ex
+++ lib/relay_server/producer.ex
@@ -1,16 +1,20 @@
 defmodule RelayServer.Producer do
   require Logger
 
-  @spec send(atom(), String.t()) :: :ok | {:error, any}
-  def send(action, _) when action not in ~w(GET GETA ADD DELETE)a do
+  @spec publish(atom(), String.t()) :: :ok | {:error, any}
+  def publish(action, _) when action not in ~w(GET GETA ADD DELETE)a do
     Logger.error("Invalid action type. Use (:GET, :GETA, :ADD, :DELETE)")
   end
 
-  def send(action, message) do
+  def publish(action, payload) do
     {:ok, conn} = AMQP.Connection.open()
     {:ok, chan} = AMQP.Channel.open(conn)
 
-    AMQP.Basic.publish(chan, "", "relay_server_queue", "#{action} #{message}")
+    json_payload = Jason.decode!(payload)
+    json_payload = json_payload |> Map.put("action", action)
+    json_payload = Jason.encode!(json_payload)
+
+    AMQP.Basic.publish(chan, "", "relay_server_queue", json_payload)
 
     AMQP.Connection.close(conn)
   end
