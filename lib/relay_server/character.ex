7346e076e1660848561035b3c9e447374cd5d552
woojiahao <woojiahao1234@gmail.com>
2021-03-14 12:57:19 +0800

    Implement to_string for Character struct

--- lib/relay_server/character.ex
+++ lib/relay_server/character.ex
@@ -2,4 +2,12 @@ defmodule RelayServer.Character do
   defstruct [:name, :age, :favorite_food]
 
   @type t :: %RelayServer.Character{name: String.t(), age: integer, favorite_food: String.t()}
+
+  defimpl String.Chars, for: RelayServer.Character do
+    def to_string(character) do
+      "Character \"#{character.name}\" is #{character.age} years old and likes #{
+        character.favorite_food
+      }"
+    end
+  end
 end
